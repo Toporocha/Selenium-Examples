b6d36f0847f4bcc8c45b053df0a1e809b324ab82
Toporocha <toporocha20@gmail.com>
2022-10-19 15:23:15 +0200

    Add amazon search file

---
+++ amazon/search.js
@@ -0,0 +1,16 @@
+const { Browser, Builder, By } = require("selenium-webdriver");
+
+(async function example() {
+  let driver = new Builder()
+    .forBrowser(Browser.CHROME)
+    .setChromeOptions(/* ... */)
+    .build();
+
+  try {
+    await driver.get("https://www.amazon.com/");
+    await driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone");
+    await driver.findElement(By.id("nav-search-submit-button")).click();
+  } finally {
+    setTimeout(() => driver.quit(), 5000);
+  }
+})();
--- youtube/search.js
+++ youtube/search.js
@@ -1,6 +1,5 @@
 const webdriver = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
-const firefox = require("selenium-webdriver/firefox");
 
 (async function example() {
   let driver = new webdriver.Builder()
@@ -13,6 +12,6 @@ const firefox = require("selenium-webdriver/firefox");
     await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
     await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
   } finally {
-    //await driver.quit();
+    setTimeout(() => driver.quit(), 5000);
   }
 })();
