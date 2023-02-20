3628bb0012a30d3d2fe020441afb1d08b96a4372
Toporocha <38571060+Toporocha@users.noreply.github.com>
2023-02-20 20:27:20 +0100

    Update search.spec.js

--- test/youtube/search.spec.js
+++ test/youtube/search.spec.js
@@ -37,35 +37,3 @@ test('search for a video on youtube', async function () {
   expect(secondResult).toBeDefined()
 })
 
-// test('Search a song on Youtube', async function example () {
-//   await driver.get('https://www.youtube.com')
-//   await driver.findElement(By.xpath('//button[. = "Accept all"]')).click()
-//   await driver.wait(until.elementLocated(By.name('search_query')))
-
-//   await driver
-//     .findElement(By.name('search_query'))
-//     .sendKeys('selenium', Key.RETURN)
-
-//await driver.findElement(By.id('search'))
-// await driver.findElement(By.id('button')).click()
-
-// await driver
-// .actions()
-// .pause(200)
-// .click(driver.findElement(By.xpath("//input[@id='search']")))
-// .pause(200)
-//.sendKeys('selenium')
-// .pause(200)
-// .sendKeys(Key.ENTER)
-//.perform()
-
-//await driver.findElement(By.id('search-input')).sendKeys('selenium webdriver')
-//await driver
-//.findElement(By.xpath("//input[@id='search']"))
-//.sendKeys('selenium webdriver')
-// await driver
-//  .findElement(webdriver.By.name('q'))
-// .sendKeys('webdriver', webdriver.Key.RETURN)
-//await driver.wait(webdriver.until.titleIs('webdriver - Google Search'), 1000)
-
-//expect(true).toBeTruthy()
