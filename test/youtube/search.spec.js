801ee88c8fc35451263fa6939f92d8e7e0287c19
Toporocha <toporocha20@gmail.com>
2023-02-20 20:20:34 +0100

    some modification

--- test/youtube/search.spec.js
+++ test/youtube/search.spec.js
@@ -1,21 +1,71 @@
-const webdriver = require('selenium-webdriver')
+const { Browser, Builder, By, Key, until } = require('selenium-webdriver')
+const { expect, test } = require('@jest/globals')
 
-;(async function example () {
-  const driver = new webdriver.Builder()
-    .forBrowser(webdriver.Browser.CHROME)
-    .setChromeOptions(/* ... */)
-    .build()
+const driver = new Builder()
+  .forBrowser(Browser.CHROME)
+  .setChromeOptions(/* ... */)
+  .build()
 
-  try {
-    await driver.get('https://www.youtube.com')
-    await driver
-      .findElement(webdriver.By.name('q'))
-      .sendKeys('webdriver', webdriver.Key.RETURN)
-    await driver.wait(
-      webdriver.until.titleIs('webdriver - Google Search'),
-      1000
-    )
-  } finally {
-    setTimeout(() => driver.quit(), 5000)
-  }
-})()
+driver
+  .manage()
+  .window()
+  .maximize()
+
+jest.setTimeout(100000)
+test('search for a video on youtube', async function () {
+  await driver.get('https://www.youtube.com/')
+  await driver.findElement(By.xpath('//button[. = "Accept all"]')).click()
+  //const searchBar =
+  await driver.wait(until.elementLocated(By.name('search_query')), 25000)
+  //await searchBar.sendKeys('video name', Key.RETURN)
+
+  await driver
+    .findElement(By.name('search_query'))
+    .sendKeys('selenium', Key.RETURN)
+
+  await driver.wait(
+    until.elementLocated(By.css('ytd-video-renderer:nth-child(5) h3'))
+  )
+  const secondResult = await driver.findElement(
+    By.css('ytd-video-renderer:nth-child(5) h3')
+  )
+  await secondResult.click()
+
+  //the below is for video results
+  // const searchResult = await driver.wait(
+  //   until.elementLocated(By.css('#video-title')))
+  expect(secondResult).toBeDefined()
+})
+
+// test('Search a song on Youtube', async function example () {
+//   await driver.get('https://www.youtube.com')
+//   await driver.findElement(By.xpath('//button[. = "Accept all"]')).click()
+//   await driver.wait(until.elementLocated(By.name('search_query')))
+
+//   await driver
+//     .findElement(By.name('search_query'))
+//     .sendKeys('selenium', Key.RETURN)
+
+//await driver.findElement(By.id('search'))
+// await driver.findElement(By.id('button')).click()
+
+// await driver
+// .actions()
+// .pause(200)
+// .click(driver.findElement(By.xpath("//input[@id='search']")))
+// .pause(200)
+//.sendKeys('selenium')
+// .pause(200)
+// .sendKeys(Key.ENTER)
+//.perform()
+
+//await driver.findElement(By.id('search-input')).sendKeys('selenium webdriver')
+//await driver
+//.findElement(By.xpath("//input[@id='search']"))
+//.sendKeys('selenium webdriver')
+// await driver
+//  .findElement(webdriver.By.name('q'))
+// .sendKeys('webdriver', webdriver.Key.RETURN)
+//await driver.wait(webdriver.until.titleIs('webdriver - Google Search'), 1000)
+
+//expect(true).toBeTruthy()
